8b03de73cebb6e4c4dacf0e08b8459470b50105c
RyheulMaxime <72041464+RyheulMaxime@users.noreply.github.com>
2021-12-23 18:33:08 +0100

    nog eens uptestloaden

--- script/app.js
+++ script/app.js
@@ -5,6 +5,64 @@ const arrNeutral = ['7','8','9'];
 var count = 0;
 var initialBet = 0;
 var ownCount = 0;
+var myChart;
+
+const labels = [
+    ''
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'How mutch should you bet',
+        backgroundColor: '#d12721',
+        borderColor: '#d12721', 
+        data: [0]
+    }]
+};
+  
+const config = {
+type: 'bar',
+data: data,
+options: {
+    indexAxis: 'y',
+    plugins: {
+        legend: false
+    },
+    scales: {
+        x: {
+            grid: {
+                color: 'white'
+            },
+            
+            min:0,
+            max: 25,
+            ticks: {
+                color: 'white',
+                stepSize: 1  
+            }
+        },
+        y:{
+            grid: {
+                color: 'white',
+                ticks: {
+                    color: 'white'
+                }
+            }, 
+        }
+    },
+    elements: {
+        bar: {
+          backgroundColor: 'rgb(0, 0, 0)',
+          borderColor: 'rgb(0, 0, 0)',
+          borderWidth: 2
+        }
+    }
+
+}
+};
+
+
 
 const get = (url) => fetch(url).then((r) => r.json());
 
@@ -23,21 +81,35 @@ let calculate_true_count = (cards_remaining) =>{
         }
         let bet = ((true_count) * initialBet)
         
-        console.log(bet)
+        console.log(bet);
+        var bedrag = [bet];
+        drawChart(bedrag);
     } else{
         console.log("no cards remaining")
     }  
 }
 
+const drawChart = (bedrag) =>{
+    const datasets  = myChart.data.datasets;
+    console.log(datasets)
+    for(const dataset of datasets ){
+      dataset.data = bedrag
+    //   console.log(myChart.data.datasets.data);
+    }
+    myChart.update();
+}
+
 const checkCount = () =>{
     ownCount = document.querySelector(".js-count").value;
-    console.log(ownCount)
+    var button = document.querySelector(".js-info-check");
+    // console.log(ownCount)
     if(ownCount==count){
         console.log("correct")
+        button.classList.remove("is-wrong");
     } else if(ownCount!=count){
         console.log("Wrong")
+        button.classList.add("is-wrong");
     }
-
 }
 
 let getCards = async (deckId) => {
@@ -108,7 +180,7 @@ let getAPI = async (deck_count) => {
     
     
 
-    const check =  document.querySelector(".js-check")
+    const check =  document.querySelector(".js-info-check")
     check.addEventListener("click",function(){
         checkCount();
     })  
@@ -116,8 +188,16 @@ let getAPI = async (deck_count) => {
 
 document.addEventListener('DOMContentLoaded', function() {
 	// 1 We will query the API with longitude and latitude.
-	initialBet = 25
-    getAPI(2);
+	initialBet = 1;
+    getAPI(6);
+
+    graph = document.querySelector(".js-graph").getContext('2d');
+
+    myChart = new Chart(
+        graph, 
+        config
+    );
+
     // console.log(deckId)
     
 });
